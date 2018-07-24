d68f2855d49dd974b3c7a7ff32f088ba793a16d9
simonleipold <simon.leipold@uzh.ch>
2018-07-24 13:24:15 +0200

    initial commit

---
+++ predictionConfig.sh
@@ -0,0 +1,7 @@
+# install latest Python 3 version from https://www.python.org/getit/
+#python3 -m venv /Users/simonleipold/Desktop/PersonalityPrediction # create a virtual environment including Python 3
+source /Users/simonleipold/Desktop/PersonalityPrediction/bin/activate # activate virtual environment
+pip install --upgrade pip # upgrade pip
+pip install numPy
+pip install scipy
+pip install -U scikit-learn
