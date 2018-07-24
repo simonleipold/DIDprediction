82e3ac1adeab0052ba2b721d0bc427595844e075
simonleipold <simon.leipold@uzh.ch>
2018-07-24 19:38:52 +0200

    updated configuration files

---
+++ ConfigSimonsiMac.sh
@@ -0,0 +1,8 @@
+# Configuration File
+# install latest Python 3 version from https://www.python.org/getit/
+#python3 -m venv /Users/simonleipold/Desktop/PersonalityPrediction # create a virtual environment including Python 3
+#source /Users/simonleipold/Desktop/PersonalityPrediction/bin/activate # activate virtual environment
+pip install --upgrade pip # upgrade pip
+pip install numpy # install numpy using pip
+pip install scipy # install scipy using pip
+pip install -U scikit-learn # install scikit-learn using pip
