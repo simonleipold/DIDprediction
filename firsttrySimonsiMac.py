2b32864a6521dad2ba4e3fcce5da5fb67da889f4
simonleipold <simon.leipold@uzh.ch>
2018-07-25 16:23:04 +0200

    new files

---
+++ firsttrySimonsiMac.py
@@ -0,0 +1,16 @@
+# scikit-learn first try
+homeDir = '/Users/simonleipold/Dropbox/NeuroTon/Matrizen4Simon' # location of the connectivity matrices
+nROIs = 4 # number of ROIs in the connectivity matrix
+import numpy as np
+import os
+from sklearn import svm
+connMatList = os.listdir(homeDir)
+connMatList = sorted(connMatList)
+samples = np.full([len(connMatList), int((nROIs*(nROIs-1))/2)], -99.0)
+for i in range(0,len(connMatList)):
+    temp = np.loadtxt(os.path.join(homeDir, connMatList[i]))
+    flatTemp = temp[np.triu_indices(n = temp.shape[1], k = 1)]
+    samples[i] = flatTemp
+
+print(samples)
+clf = svm.SVC(gamma=0.001, C=100.)
